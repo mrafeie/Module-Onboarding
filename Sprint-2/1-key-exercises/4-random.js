71bd055714e94613755d5f70be2d2de78a5ee434
mrafeie <m.rafeie.mr@gmail.com>
2026-09-06 20:54:15 +0100

    Clarify random number generation in 4-random.js
    Added comments explaining the random number generation logic.

--- Sprint-2/1-key-exercises/4-random.js
+++ Sprint-2/1-key-exercises/4-random.js
@@ -7,3 +7,9 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+
+//num represents a random whole number between 1 and 100
+// Math.random() produces a random decimal from 0 up to 1 .
+// Math.random() * 100 produces a decimal from 0 up to 100.
+// Math.floor() rounds the number down to a whole number, giving 0–99.
+// +minimum (+1) shifts that range to 1–100.
