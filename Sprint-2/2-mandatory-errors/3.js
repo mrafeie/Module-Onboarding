0ae360a095d5529a45ebdcf28fc1b011e3e35667
mrafeie <m.rafeie.mr@gmail.com>
2026-09-06 21:07:55 +0100

    Fix last4Digits assignment to use string conversion
    Convert cardNumber to string before slicing to get last 4 digits.

--- Sprint-2/2-mandatory-errors/3.js
+++ Sprint-2/2-mandatory-errors/3.js
@@ -1,5 +1,5 @@
 const cardNumber = 4533787178994213;
-const last4Digits = cardNumber.slice(-4);
+const last4Digits = String(cardNumber).slice(-4);
 
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
@@ -7,3 +7,5 @@ const last4Digits = cardNumber.slice(-4);
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
+
+// The .slice() method works on strings, but numbers don't have a .slice() method
