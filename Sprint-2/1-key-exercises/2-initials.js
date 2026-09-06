2ce1dda2fb1431c32dd01dde3329678ce8fd3044
mrafeie <m.rafeie.mr@gmail.com>
2026-09-06 20:38:02 +0100

    Store initials from first, middle, and last names

--- Sprint-2/1-key-exercises/2-initials.js
+++ Sprint-2/1-key-exercises/2-initials.js
@@ -5,6 +5,6 @@ const lastName = "Johnson";
 // Declare a variable called initials that stores the first character of each string.
 // This should produce the string "CKJ", but you must not write the characters C, K, or J in the code of your solution.
 
-const initials = ``;
+const initials = firstName[0] + middleName[0] + lastName[0];
 
 // https://www.google.com/search?q=get+first+character+of+string+mdn
