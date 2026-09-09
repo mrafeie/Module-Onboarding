9485482e5898149c32122bff58514f8837bd781c
mrafeie <m.rafeie.mr@gmail.com>
2026-09-09 17:59:03 +0100

    Fix parameter name in square function
    Corrected the parameter name in the square function to avoid SyntaxError.

--- Sprint-3/1-key-errors/2.js
+++ Sprint-3/1-key-errors/2.js
@@ -4,17 +4,23 @@
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
+// A SyntaxError will occur because 3 is not a valid parameter name.
 
 function square(3) {
     return num * num;
 }
 
 // =============> write the error message here
+// SyntaxError: Unexpected number
 
 // =============> explain this error message here
+// A parameter must be an identifier (variable name), but 3 is a number.
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
+function square(num) {
+    return num * num;
+}
 
 
