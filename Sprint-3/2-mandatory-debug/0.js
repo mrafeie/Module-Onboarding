7b557c29e7c7a3ecbb76390e5e87ea9fe8335a7d
mrafeie <m.rafeie.mr@gmail.com>
2026-09-09 18:05:05 +0100

    Fix multiply function to return result
    Updated the multiply function to return the product of two numbers.

--- Sprint-3/2-mandatory-debug/0.js
+++ Sprint-3/2-mandatory-debug/0.js
@@ -1,6 +1,9 @@
 // Predict and explain first...
 
 // =============> write your prediction here
+// The output will be:
+// 320
+// The result of multiplying 10 and 32 is undefined
 
 function multiply(a, b) {
   console.log(a * b);
@@ -9,6 +12,14 @@ function multiply(a, b) {
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
 // =============> write your explanation here
+// multiply(10, 32) runs the function and console.log(a * b) prints 320.
+// However, the function does not have a return statement.
+// A JavaScript function without a return statement returns undefined.
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function multiply(a, b) {
+  return a * b;
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
