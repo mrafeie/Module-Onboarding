d1e02fefd14e402a81d1e3498c09038515443721
mrafeie <m.rafeie.mr@gmail.com>
2026-09-09 18:33:41 +0100

    Document pad function behavior in time-format.js
    Added comments with answers to questions about the pad function in time-format.js.

--- Sprint-3/4-mandatory-interpret/time-format.js
+++ Sprint-3/4-mandatory-interpret/time-format.js
@@ -22,17 +22,30 @@ function formatTimeDisplay(seconds) {
 
 // a) When formatTimeDisplay is called how many times will pad be called?
 // =============> write your answer here
+// 3
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
 // b) What is the value assigned to num when pad is called for the first time?
 // =============> write your answer here
+// num = 0
 
 // c) What is the return value of pad when it is called for the first time?
 // =============> write your answer here
+// "00"
 
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
 // =============> write your answer here
+// num = 1
+// The three calls are:
+// pad(totalHours)       -> pad(0)
+// pad(remainingMinutes) -> pad(1)
+// pad(remainingSeconds) -> pad(1)
+// Therefore, the last call receives 1 as num.
 
 // e) What is the return value of pad when it is called for the last time in this program?  Explain your answer
 // =============> write your answer here
+// "01"
+// num is 1, so numString starts as "1".
+// Because its length is less than 2, "0" is added to the beginning.
+// The function returns "01".
