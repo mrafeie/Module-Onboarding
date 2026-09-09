8f7062f9ace56444d241128091f24bdae8964cab
mrafeie <m.rafeie.mr@gmail.com>
2026-09-09 18:19:12 +0100

    Add function to convert string to upper snake case

--- Sprint-3/3-mandatory-implement/2-cases.js
+++ Sprint-3/3-mandatory-implement/2-cases.js
@@ -14,3 +14,6 @@
 // You will need to come up with an appropriate name for the function
 // Use the MDN string documentation to help you find a solution
 // This might help https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
+function toUpperSnakeCase(str) { 
+  return str.toUpperCase().replaceAll(" ", "_");
+}
