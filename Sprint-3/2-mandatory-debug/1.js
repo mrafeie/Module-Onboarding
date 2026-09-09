5d7a4cf34d063ea7515087077dce3f7e4b19e8d7
mrafeie <m.rafeie.mr@gmail.com>
2026-09-09 18:07:33 +0100

    Correct sum function to return a + b
    Fixed the sum function to return the correct sum of two numbers.

--- Sprint-3/2-mandatory-debug/1.js
+++ Sprint-3/2-mandatory-debug/1.js
@@ -1,5 +1,6 @@
 // Predict and explain first...
 //  =============> write your prediction here
+// Output: The sum of 10 and 32 is undefined
 
 function sum(a, b) {
   return;
@@ -9,5 +10,12 @@ function sum(a, b) {
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
+// Because there is no value after `return`, the function returns undefined.
+// The line a + b is unreachable code, meaning it will never run.
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
